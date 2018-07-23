83a43633d2b64421b5e7b6d32ecf3b17a3b2d79a
David J. Neu <david@davidneu.com>
2018-07-23 11:05:10 -0400

    Fix filename in set-project-name.sh.

--- set-project-name.sh
+++ set-project-name.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $# -eq 1 ]; then
-    sed -i "s/myapp/$1/g" README.org
+    sed -i "s/myapp/$1/g" README.md
     sed -i "s/myapp/$1/g" html/custom_404.html
     sed -i "s/myapp/$1/g" html/custom_50x.html
 else
